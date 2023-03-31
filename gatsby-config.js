a69b3e14734520f8d688a3faf8e86ae149dc2fc3
Sungkuk Park <sungkuk.park@wooga.net>
2023-03-31 17:35:36 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Sungkuk Park's blog`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
